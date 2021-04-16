3694c003e57864c55ee2c5620c3371e05784d2e2
rockexe0000 <rockexe0000@yahoo.com.tw>
2021-04-16 09:54:58 +0800

    update

--- src/main/java/com/zaifu/SpringBootWeb/SpringBootWebApplication.java
+++ src/main/java/com/zaifu/SpringBootWeb/SpringBootWebApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zaifu.SpringBootWeb;
 
 import org.springframework.boot.SpringApplication;
@@ -28,18 +27,3 @@ public class SpringBootWebApplication {
 	}
 
 }
-=======
-package com.zaifu.SpringBootWeb;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringBootWebApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
-
-}
->>>>>>> main
--- src/test/java/com/zaifu/SpringBootWeb/SpringBootWebApplicationTests.java
+++ src/test/java/com/zaifu/SpringBootWeb/SpringBootWebApplicationTests.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zaifu.SpringBootWeb;
 
 import org.junit.jupiter.api.Test;
@@ -12,18 +11,3 @@ class SpringBootWebApplicationTests {
 	}
 
 }
-=======
-package com.zaifu.SpringBootWeb;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootWebApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
->>>>>>> main
