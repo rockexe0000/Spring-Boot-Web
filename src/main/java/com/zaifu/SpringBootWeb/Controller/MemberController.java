ace2b3145318fd967dbf787c419eec574589e157
rockexe0000 <rockexe0000@yahoo.com.tw>
2021-04-16 17:57:29 +0800

    redis 還沒設定好

--- src/main/java/com/zaifu/SpringBootWeb/Controller/MemberController.java
+++ src/main/java/com/zaifu/SpringBootWeb/Controller/MemberController.java
@@ -1,12 +1,22 @@
 package com.zaifu.SpringBootWeb.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zaifu.SpringBootWeb.Dao.MemberRepository;
 import com.zaifu.SpringBootWeb.Model.MemberAccount;
+import com.zaifu.SpringBootWeb.Model.MemberAccountJPA;
 import com.zaifu.SpringBootWeb.Service.MemberService;
 
 
@@ -14,11 +24,21 @@ import com.zaifu.SpringBootWeb.Service.MemberService;
 @Controller
 public class MemberController {
 	//透過 @RequestMapping 指定從/會被對應到此hello()方法
-	@Autowired
-	MemberAccount memberAccount;
+//	@Autowired
+//	MemberAccount memberAccount;
+//	
+//	@Autowired
+//	MemberService memberService;
+	
 	
 	@Autowired
-	MemberService memberService;
+	MemberRepository memberRepository;
+	
+	@Autowired
+	DataSource dataSource;
+	
+	
+	
 	
 	/*
     @RequestMapping("/")
@@ -35,13 +55,45 @@ public class MemberController {
     
 	@GetMapping("/addMemberPage")
     public String addMemberPage(){
-//    	memberAccount = new MemberAccount();
-//    	memberService.addMember(memberAccount);
 
+		List<MemberAccountJPA> memberAccountJPA= new ArrayList<MemberAccountJPA>();
+		memberAccountJPA = memberRepository.findAll();
+
+		for(int i=0;i<memberAccountJPA.size();i++){
+			System.out.println(memberAccountJPA.get(i).getId());
+		}
         return "addMemberPage";
     }
     
     
-    
+	@GetMapping("/login")
+    public String login(@ModelAttribute MemberAccountJPA memberAccountJPA){
+
+        return "login";
+    }
+	
+	@PostMapping("/doLogin")
+    public String doLogin(@ModelAttribute MemberAccountJPA memberAccountJPA,HttpSession session){
+		String email = memberAccountJPA.getEmail();
+		String password = memberAccountJPA.getPassword();
+		List<MemberAccountJPA> MemberAccountJPAList = new ArrayList<MemberAccountJPA>();
+		MemberAccountJPAList = memberRepository.findCheckMemberAccount(email, password);
+		MemberAccount memberAccount = new  MemberAccount();
+		memberAccount.setPassword(password);
+		memberAccount.setEmail(email);;
+
+		if(MemberAccountJPAList.size()==0){
+			return "login";
+		}
+		else{
+			session.setAttribute("uid", memberAccount);
+	        return "welcome";
+		}
+
+    }
+	
+	
+	
+	
     
 }
---
+++ src/main/java/com/zaifu/SpringBootWeb/RedisSessionConfig.java
@@ -0,0 +1,30 @@
+package com.zaifu.SpringBootWeb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
+public class RedisSessionConfig {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
